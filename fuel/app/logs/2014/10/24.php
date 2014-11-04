a9de3fa8fafe978f081ed05eca43773a590e2579
bt-kishihara <d_kishihara@brain-tr.co.jp>
2014-11-04 20:40:42 +0900

    initial commit

---
+++ fuel/app/logs/2014/10/24.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-10-24 11:59:21 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
