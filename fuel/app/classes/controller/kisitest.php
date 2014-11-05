9728bfc89f2fbe9a1a7f8846262e2555b364b332
bt-kishihara <d_kishihara@brain-tr.co.jp>
2014-11-05 21:57:30 +0900

    色々間違えてたので修正

--- fuel/app/classes/controller/kisitest.php
+++ fuel/app/classes/controller/kisitest.php
@@ -9,7 +9,8 @@ class Controller_Kisitest extends Controller
 	{
 		$data["point"]			= mt_rand(1,99);
 		$data["create_date"]	= date("Y-m-d H:i:s");
-		db_project::get_project_id_kind($post["find_ids"]);
+		db_ranking::ins_ranking($data);
+		exit;
 	}
 
 
