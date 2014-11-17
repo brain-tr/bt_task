daf72c1a175c6bc56b542cfa6438fb6f756c34fc
bt-uemura <m_uemura@brain-tr.co.jp>
2014-11-17 18:09:28 +0900

    no message

--- fuel/app/views/user/close.php
+++ fuel/app/views/user/close.php
@@ -1,7 +1,6 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>エンジニア対応管理システム</title>
 	<link href="/assets/css/common.css" rel="stylesheet" type="text/css" media="all" />
 	<script type="text/javascript">
